6a2f1d34290e4d158cab172497f5a05c4f22727a
kindex <kindexz@gmail.com>
2024-01-11 14:12:36 +0200

    Deploy script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,17 +1,20 @@
 import os
+from brownie import *
 
 PUBLISH_SOURCES=True
 
 private_key=os.getenv('DEBTCOIN_DEPLOYER_PRIVATE_KEY')
 accounts.add(private_key)
 
+
+DEPLOYER=accounts[len(accounts)-1]
+OWNER=''
 HOLDER=''
 TARGET_ACCOUNT=''
-OWNER=''
 
 def main():
-    print('Deployer account= {}'.format(accounts[0]))
+    print('Deployer account= {}'.format(DEPLOYER))
 
-    debtcoin = Debtcoin.deploy(HOLDER_ACCOUNT, {'from':accounts[0]}, publish_source=PUBLISH_SOURCES)
+    debtcoin = Debtcoin.deploy(HOLDER, {'from':DEPLOYER}, publish_source=PUBLISH_SOURCES)
 
-    vesting = Vesting.deploy(debtcoin, {'from':accounts[0]}, publish_source=PUBLISH_SOURCES)
+    vesting = Vesting.deploy(debtcoin, OWNER, TARGET_ACCOUNT, {'from':DEPLOYER}, publish_source=PUBLISH_SOURCES)
